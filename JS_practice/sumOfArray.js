efc4b5ca1eb7d710bc898399f69036e0ff1d3418
vermamaan55@gmail.com <vermamaan55@gmail.com>
2025-12-29 12:09:43 +0530

    Added sumOfArray.js, Calculates the Sum of an Array.

---
+++ JS_practice/sumOfArray.js
@@ -0,0 +1,16 @@
+const prompt = require("prompt-sync")();
+
+let x = Number(prompt("Enter length of an Array :"));
+let arr = Array.of(x);
+
+for(var i=0;i<x;i++){
+    var z = Number(prompt("Enter Elements of Array:"));
+    arr.push(z);
+}
+
+var sum = 0;
+for(var y=0;y<x;y++){
+    sum += arr.pop(y);
+}
+
+console.log(sum);
