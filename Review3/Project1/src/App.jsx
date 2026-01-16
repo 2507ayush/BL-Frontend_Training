179cee997d2bcd38a425ac60df7267c09474b586
vermamaan55@gmail.com <vermamaan55@gmail.com>
2026-01-16 16:21:48 +0530

    Created a Counter with Increase, Decrease and Reset functionality.

--- Review3/Project1/src/App.jsx
+++ Review3/Project1/src/App.jsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+class Counter extends React.Component {
+  state = {count: 0}
 
+  increment = () => {
+    this.setState((prev) => ({
+      count:
+        (prev.count % 2 === 0 && prev.count !== 0) 
+          ? prev.count + 2
+          : prev.count + 1
+    }))
+  }
+
+  decrement = () => {
+    this.setState((prev) => ({
+      count: prev.count > 0 ? prev.count - 1 : 0
+    }))
+  }
+
+  reset = () => {
+    this.setState({ count: 0 })
+  }
+
+  render() {
+    const { count } = this.state;
+
+    return (
+      <>
+        <h2>Counter: {count}</h2>
+
+        <button className="Inc" onClick={this.increment}>
+          Increment
+        </button>
+
+        <button className="Dec" onClick={this.decrement}>
+          Decrement
+        </button>
+
+        <button className="Reset" onClick={this.reset}>
+          Reset
+        </button>
+
+        <p>
+          {count > 10 ? "High Value" : "Low Value"}
+        </p>
+      </>
+    )
+  }
+}
+
+function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Counter />
     </>
   )
 }
 
-export default App
+export default App;
